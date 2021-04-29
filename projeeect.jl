ddac3431c09b40e3ee31f7cf7b0088531bfca1f3
Sergei Raikov <255908@student.pwr.edu.pl>
2021-04-29 19:47:45 +0300

    Kolejny commit z zachowaniem do list

--- projeeect.jl
+++ projeeect.jl
@@ -2,7 +2,7 @@ using AstroLib
 
 day_sec = 86400
 
-planet_names = ["earth",]
+planet_names = ["earth", "mars", "venus", "jupiter"]
 
 function get_mass(planet_names)
    [AstroLib.planets[i].mass for i in planet_names]
@@ -23,3 +23,11 @@ end
 function get_eccentricity(planet_names) # mimosrod 
     [AstroLib.planets[i].ecc for i in planet_names]
 end
+
+
+masses = get_mass(planet_names)
+radiuses = get_radius(planet_names)
+semi_maj_ax = get_semi_ma_axis(planet_names)
+periods = get_period(planet_names)
+ecc = get_eccentricity(planet_names)
+m_anomaly = mean_anomaly(planet_names)
