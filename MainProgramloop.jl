843f87b31b411e8874c7b4efecb50d7c7f56118c
Wojciech Dwornik <244585@student.pwr.edu.pl>
2021-05-03 21:22:25 +0200

    działa

--- MainProgramloop.jl
+++ MainProgramloop.jl
@@ -10,7 +10,7 @@ G = 6.6732*10^(-11)     # stała grawitacyjna
 Planeta1 = Dict("M" => 10^16, "coord" => [0,10000], "v_vec" => [20,0])
 Planeta2 = Dict("M" => 10^17, "coord" => [0,0], "v_vec" => [0,0])
 Planeta3 = Dict("M" => 10^16, "coord" => [0,-10000],"v_vec" =>[-20,0])
-lista=[Planeta1, Planeta2]
+lista=[Planeta1, Planeta2, Planeta3]
 #---------------------------------------------------------------------------------------------
 #       FUNKCJE DO OBLICZEŃ 
 #---------------------------------------------------------------------------------------------
@@ -21,23 +21,25 @@ dist_vec(coord1,coord2) = coord2 .- coord1
 vec_length(v::Array) = sqrt(sum(v.^2))
 """Wylicza długość wektora"""
 
-F_gravity(M1::Number,M2::Number,r::Array) = (G*M1*M2/vec_length(r)^3)*r
+F_gravity(M1::Number,M2::Number,r::Array) = (G*M1*M2/vec_length(r)^3).*r
 """Wylicza wektor siły grawitacji"""
 
 
 function MainFunction(l,t::Number)
     for i in 1:length(lista)
-        FG=0
-        for j in 1:length(lista) if j!=i
-            r=dist_vec(get(lista[i],"coord",1),get(lista[j],"coord",1))
-            F=F_gravity(get(lista[i],"M",1),get(lista[j],"M",1),r)
-            FG .+= F
+        FG=[0.0,0.0]
+        for j in 1:length(lista)
+            if j!=i
+                r=dist_vec(get(lista[i],"coord",1),get(lista[j],"coord",1))
+                F=F_gravity(get(lista[i],"M",1),get(lista[j],"M",1),r)
+                FG .+= F
+            end
         end
-        a_vec=FG/lista[i]["M"]
-        lista[i]["v_vec"]=a_vec*t+lista[i]["v_vec"]
+        a_vec=FG./lista[i]["M"]
+        lista[i]["v_vec"]=a_vec.*t.+lista[i]["v_vec"]
     end
     for i in 1:length(lista)
-        lista[i]["coord"]=lista[i]["coord"]+lista[i]["v_vec"]
+        lista[i]["coord"]=lista[i]["coord"].+lista[i]["v_vec"]
     end
         
 
@@ -71,9 +73,14 @@ coordy_P1 = []
 coordx_P2 = []
 coordy_P2 = []
 
+coordx_P3 = []
+coordy_P3 = []
+
 for i in t
     append!(coordx_P1, Planeta1["coord"][1])
     append!(coordy_P1, Planeta1["coord"][2])
+    append!(coordx_P2, Planeta2["coord"][1])
+    append!(coordy_P2, Planeta2["coord"][2])
     append!(coordx_P2, Planeta2["coord"][1])
     append!(coordy_P2, Planeta2["coord"][2])
     MainFunction(Planeta1,Planeta2,T)
@@ -99,3 +106,4 @@ symulation = @animate for i in t
 end
 
 gif(symulation,"animacja.gif",fps=fps)
+
